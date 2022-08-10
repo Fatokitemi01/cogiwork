f800577d24fda12c97350cfa9858862e80b850ce
Fatokitemi01 <fatokitemi01@gmail.com>
2022-08-10 20:08:34 +0000

    First Commit
    Home page and navbar almost completed

---
+++ CogiWork/main-finbuzz/finbuzz-placeholder/header.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>COGI </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon Start Here -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/fav.png">
+    <!-- Bootstrap Css Start Here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Animate Css Start Here -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Owl Carousel Start Here -->
+    <link rel="stylesheet" href="vendor/owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendor/owlcarousel/owl.theme.default.min.css">
+    <!-- Nivo Slider Start Here -->
+    <link rel="stylesheet" href="vendor/nivo-slider/nivo-slider.css">
+    <!-- Sal Css Start Here -->
+    <link rel="stylesheet" href="css/sal.css" type="text/css">
+    <!-- Animate Css Start Here -->
+    <link rel="stylesheet" href="css/animate.min.css">
+    <!-- Slick Css Start Here -->
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+   <!-- Meanmenu Css Start Here -->
+    <link rel="stylesheet" href="css/meanmenu.min.css">
+    <!-- All min Css Start Here -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!-- Style Css Start Here -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Google Font Start Here -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+<body class="sticky-header">
+    <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+    <!--=====================================-->
+    <!--=   Scroll-up     Start      blue---#4169a3  red ##cd3539         =-->
+    <!--=====================================-->
+    <a href="#wrapper" data-type="section-switch" class="scrollup">
+        <i class="fas fa-angle-double-up"></i>
+    </a>
+    <!--=====================================-->
+    <!--=   Preloader     Start             =-->
+    <!--=====================================-->
+    <div id="preloader"></div>
+    <div class="wrapper" id="wrapper">
+        <!--=====================================-->
+        <!--=            Header Start           =-->
+        <!--=====================================-->
+        <header class="header">
+            <div id="header-topbar" class="header-topbar-layout1">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6">
+                            <div class="topbar-left">
+                                <p class="item-paragraph">Are you ready to grow up your business?</p>
+                                <div class="header-button">
+                                    <a href="#">Join our team <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 d-flex justify-content-end">
+                            <div class="topbar-right">
+                                <ul>
+                                    <li class="topbar-social">
+                                        <div class="social-icon">
+                                            <a href="#"><i class="fab fa-facebook-square"></i></a> 
+                                            <a href="#"><i class="fab fa-twitter"></i></a>
+                                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                            <a href="#"><i class="fab fa-pinterest"></i></a>
+                                            <a href="#"><i class="fab fa-skype"></i></a>
+                                        </div>
+                                    </li>
+                                </ul>
+                                <div class="header-right-button">
+                                    <a href="#" class="header-btn">Find An Advicer</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="rt-sticky-placeholder"></div>
+            <div id="header-menu" class="header-menu menu-layout1">
+                <div class="container">
+                    <div class="row d-flex align-items-center">
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo-area"style=" padding-right: 30px;">
+                                <a href="index.html" class="temp-logo">
+                                    <img src="img/logo/logo.jpg" alt="logo" class="img-fluid">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-7 d-flex justify-content-end position-static">
+                            <nav id="dropdown" class="template-main-menu">
+                                <ul>
+                                   <li>
+                                        <a href="index.html">Home</a>
+                                        
+                                    </li>
+                                    <li>
+                                        <a href="about.html">About </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Services</a>
+                                        <ul class="dropdown-menu-col-1">
+                                            <li>
+                                                <a id="aa" href="service1.html">COGI Publication</a>
+                                            </li>
+                                            <li>
+                                                <a href="service1.html">COGI IT Consulting</a>
+                                            </li>
+                                            <li>
+                                                <a href="service1.html">COGI real estate & COG1 Rental Properties</a>
+                                            </li>
+                                            <li>
+                                                <a href="service1.html">COGI Leadership Development Center</a>
+                                            </li>
+                                            <li>
+                                                <a href="service1.html">COGI Resort</a>
+                                            </li>
+                                           
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="team1.html">Management Team</a>
+                                      
+                                    </li>
+                                    <li class="position-static hide-on-mobile-menu">
+                                        <a href="portfolio1.html">Portfolio</a>
+                                        
+                                    </li>
+                                    <li class="hide-on-desktop-menu">
+                                        <a href="#">Pages</a>
+                                        <ul>
+                                            <li>
+                                                <a href="about1.html">About</a>
+                                            </li>
+                                            <li>
+                                                <a href="service1.html">Services</a>
+                                            </li>
+                                            <li>
+                                                <a href="project1.html">Project</a>
+                                            </li>
+                                            <li>
+                                                <a href="404.html">404 Error</a>
+                                            </li>
+                                            <li>
+                                                <a href="contact.html">Contact</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="contact.html">Contact</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                      
